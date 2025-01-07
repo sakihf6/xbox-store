30feccc05a99713992bb33893a115b462898f7b4
sakihf6 <sakihf6@gmail.com>
2025-01-07 17:32:38 -0600

    diossssss

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import string
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 database_url = os.environ.get('DATABASE_URL', "postgresql://xbox_m4o1_user:rRieocXzonRdTslrkyDRfgrPp5a1Sc02@dpg-ctuphf56l47c738nk2h0-a.oregon-postgres.render.com/xbox_m4o1")
 if database_url.startswith("postgres://"):
